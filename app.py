4f091180960ab1d94dafaef88fb50e5bbf58d9f7
i <arcliya@Gmail.com>
2018-06-29 19:08:22 +0300

    adding html

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+import sshtunnel
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = r"mysql+pymysql://sql7245242:YzvNeHxF8X@sql7.freemysqlhosting.net:3306/sql7245242"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    __tablename__ = 'Games'
+    id = db.Column('id',db.Integer,primary_key=True)
+    team1 = db.Column('team1',db.VARCHAR)
+    team2 = db.Column('team2', db.VARCHAR)
+
+    def __init__(self,id,team1,team2):
+        self.id = id
+        self.team1 = team1
+        self.team2 = team2
 
 @app.route('/')
+def hello():
+    return '<h2>testing<h2>'
+
+
+@app.route('/test')
 def hello_world():
     string = 'roman is in the house'
     return 'Hello World! {} HEY HEY'.format(string)
