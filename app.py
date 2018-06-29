9ed91c52ae0ba4762064f5ea9ba43115c50739c7
i <arcliya@Gmail.com>
2018-06-29 11:19:57 +0300

    testing roman1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     string = 'roman is in the house'
-    return 'Hello World! {}'.fomrat(string)
+    return 'Hello World! {}'.format(string)
 
 
 
